47b3691e95a384064db2e196753eef1e08fc37c1
조인화 <ihnhwa8362@naver.com>
2024-03-11 14:11:35 +0900

    [BE] Setting: 프로젝트 생성

---
+++ backend/src/main/java/com/s005/fif/FifApplication.java
@@ -0,0 +1,13 @@
+package com.s005.fif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FifApplication.class, args);
+	}
+
+}

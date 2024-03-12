dd0cccdd1b37d855dc6824940e8bd688ba04c869
조인화 <ihnhwa8362@naver.com>
2024-03-12 11:02:09 +0900

    [BE] Setting: 지병별 주의 식재료 entity에 설명 field 추가

--- backend/src/main/java/com/s005/fif/entity/CautionIngredientRel.java
+++ backend/src/main/java/com/s005/fif/entity/CautionIngredientRel.java
@@ -1,5 +1,7 @@
 package com.s005.fif.entity;
 
+import com.s005.fif.common.Constant;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +26,9 @@ public class CautionIngredientRel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer cautionIngredientRelId;
+
+	@Size(max = Constant.COMMON_CONTENT_LENGTH)
+	private String description;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "ingredient_id", nullable = false)

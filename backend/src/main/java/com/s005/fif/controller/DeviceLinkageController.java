faa6f87858e5a740c5b2594bc7535e89ef90809d
곽미래 <alfo9490@naver.com>
2024-03-19 15:23:16 +0900

    [BE] Fix #22: 토큰 파싱을 위한 controller 파라미터에 MemberDto 추가

--- backend/src/main/java/com/s005/fif/controller/DeviceLinkageController.java
+++ backend/src/main/java/com/s005/fif/controller/DeviceLinkageController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.s005.fif.common.auth.MemberDto;
 import com.s005.fif.common.response.Response;
 import com.s005.fif.service.DeviceLinkageService;
 
@@ -19,23 +20,20 @@ public class DeviceLinkageController {
 	private final DeviceLinkageService deviceLinkageService;
 
 	@PostMapping("/recipes/{recipeId}/galaxy-watch")
-	public Response connectWithGalaxyWatch(@PathVariable Integer recipeId) {
-		Integer memberId = 1;
-		deviceLinkageService.connectWithGalaxyWatch(memberId, recipeId);
+	public Response connectWithGalaxyWatch(MemberDto memberDto, @PathVariable Integer recipeId) {
+		deviceLinkageService.connectWithGalaxyWatch(memberDto.getMemberId(), recipeId);
 		return new Response(Response.MESSAGE, "연동되었습니다.");
 	}
 
 	@GetMapping("/recipes/{recipeId}/galaxy-watch")
-	public Response disconnectWithGalaxyWatch(@PathVariable Integer recipeId) {
-		Integer memberId = 1;
-		deviceLinkageService.disconnectWithGalaxyWatch(memberId, recipeId);
+	public Response disconnectWithGalaxyWatch(MemberDto memberDto, @PathVariable Integer recipeId) {
+		deviceLinkageService.disconnectWithGalaxyWatch(memberDto.getMemberId(), recipeId);
 		return new Response(Response.MESSAGE, "연동이 종료되었습니다.");
 	}
 
 	@GetMapping("/api/recipes/step/{step}")
-	public Response moveToNextStep(@PathVariable Integer step) {
-		Integer memberId = 1;
-		deviceLinkageService.moveToNextStep(memberId, step);
+	public Response moveToNextStep(MemberDto memberDto, @PathVariable Integer step) {
+		deviceLinkageService.moveToNextStep(memberDto.getMemberId(), step);
 		return new Response(Response.MESSAGE,  step +" 단계로 이동합니다.");
 	}
 }

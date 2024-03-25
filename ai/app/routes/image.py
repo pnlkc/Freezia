300f20c8c60686e8cfcbaba5d6cd8d9b6acc06ed
조인화 <ihnhwa8362@naver.com>
2024-03-25 10:08:48 +0900

    [BE] Feat #41: 이미지 생성 API 구현

--- ai/app/routes/image.py
+++ ai/app/routes/image.py
@@ -3,20 +3,38 @@ from common import client
 
 image_router = APIRouter()
 
-
-@image_router.get("/")
+@image_router.get("")
 def generate_image(
-    recipe_name: str,
-    # ingredients: str
+    recipeName: str,
+    ingredients: str,
+    recipeTypes: str,
 ):
     try:
         prompt = f"""
-        맛있게 만들어진 {recipe_name} 사진을 만들어 줘.
+        음식 사진를 만들어 줘.
+        음식 : {recipeName}
+        식재료 : {ingredients}
+        음식 유형 : {recipeTypes}
         
-        다음 조건을 반드시 만족해야 해!
-        - 화목한 가정의 따뜻한 분위기가 나야 해.
+        식재료를 알려주는 이유는 음식이라는 피사체에 반영하고 싶기 때문입니다.
+        그러니까 절대로 식재료를 음식과 별개로 따로 나열하지 마십시오.
+        식재료는 원래 형태대로 음식에 담기지 않고 조리된 상태로 음식에 들어갑니다.
+        식재료는 원래 크기대로 음식에 담기지 않고 갓난 아기가 먹을 수 있을 정도로 매우 작은 크기로 들어갑니다.
+        음식만 있고 식재료는 보이지 않는 이미지.
         
-        음식과 관련 없는 질문에는 절대 대답하지마.
+        음식은 원목 소재의 식탁 위의 정가운데에 놓여있습니다.
+        누군가가 그린 그림이 아닌 음식 사진 전문 사진가가 실제로 찍은 듯한 이미지.
+        당장 입에 넣고 싶을 정도로 맛있어 보이는 사진.
+        
+        식욕을 자극하는 색감.
+        음식에서 윤기가 납니다.
+        따뜻한 음식이라면 김이 모락모락 나는 것을 표현해줘.
+        
+        음식을 비스듬히 내려다보는 구도.
+        음식 전체가 사진 안에 포함되어 있는 구도.
+        
+        Create an image so lip-smackingly impressive that it instantly triggers an appetite.
+        Give warm-touch to the food by showing the steam wafting up from it, suggesting it's freshly cooked and hot.
         """
 
         response = client.images.generate(
@@ -29,7 +47,7 @@ def generate_image(
             style="natural"     # vivid or natural
         )
 
-        print(response.data[0].url)
+        # print(response.data[0].url)
         print(response.data[0].revised_prompt)
 
         return response.data[0].url

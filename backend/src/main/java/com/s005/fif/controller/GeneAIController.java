a26ef37d5e61a72e9c72356882298c25e8a5ec0d
이주혁 <wngur4300@gmail.com>
2024-03-25 14:38:22 +0900

    [BE] Fix #54: FastAPI 운영서버 URL 추가 및 requirements.txt 수정

--- backend/src/main/java/com/s005/fif/controller/GeneAIController.java
+++ backend/src/main/java/com/s005/fif/controller/GeneAIController.java
@@ -20,13 +20,6 @@ public class GeneAIController {
 
     private final GeneAIService geneAIService;
 
-    @GetMapping("/websocket")
-    @Operation(summary = "테스트용")
-    public Response getThreadAndAssistant() {
-        geneAIService.get();
-        return new Response("fridgeIngredients", 1);
-    }
-
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @Operation(summary = "대화형 레시피 추천")
     public Flux<String> streamDataFromAI(GeneAIPromptRequestDto geneAIPromptRequestDto) {
@@ -37,31 +30,5 @@ public class GeneAIController {
         // 받아온 스트림을 그대로 클라이언트에 전송
         return fastApiStream;
     }
-
-//    @GetMapping
-//    public SseEmitter streamDataFromAI() throws IOException {
-//        SseEmitter emitter = new SseEmitter();
-//
-//        // FastAPI에서 SSE 스트림을 받는 로직
-//        Flux<String> fastApiStream = webClientService.getStreamDataFromAI();
-//
-//        fastApiStream.subscribe(
-//                // 데이터를 받을 때마다 클라이언트에 데이터 전송
-//                data -> {
-//                    try {
-//                        emitter.send(data);
-//                    } catch (IOException e) {
-//                        emitter.completeWithError(e);
-//                    }
-//                },
-//                // 에러 처리
-//                error -> emitter.completeWithError(error),
-//                // 스트림 완료 처리
-//                () -> emitter.complete()
-//        );
-//
-//        return emitter;
-//    }
-
 
 }

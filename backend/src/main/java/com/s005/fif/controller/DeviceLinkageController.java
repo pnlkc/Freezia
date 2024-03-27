890d8a37b8838e7b0acecd6da323a7bff7df1714
김지민 <mnvyiions@gmail.com>
2024-03-27 13:24:39 +0900

    [BE] Fix #61: FCM 토큰값 수정 API 추가

--- backend/src/main/java/com/s005/fif/controller/DeviceLinkageController.java
+++ backend/src/main/java/com/s005/fif/controller/DeviceLinkageController.java
@@ -3,11 +3,13 @@ package com.s005.fif.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.s005.fif.common.auth.MemberDto;
 import com.s005.fif.common.response.Response;
+import com.s005.fif.dto.fcm.FcmTokenDto;
 import com.s005.fif.service.DeviceLinkageService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -41,4 +43,12 @@ public class DeviceLinkageController {
 		deviceLinkageService.moveToNextStep(memberDto.getMemberId(), step);
 		return new Response(Response.MESSAGE,  step +" 단계로 이동합니다.");
 	}
+
+	@PostMapping("/fcm/token")
+	@Operation(summary = "디바이스 토큰 갱신")
+	public Response save(@Parameter(hidden = true) MemberDto memberDto, @RequestBody FcmTokenDto fcmTokenDto) {
+		deviceLinkageService.saveToken(memberDto.getMemberId(), fcmTokenDto);
+		return new Response(Response.MESSAGE, "새로운 토큰이 등록 되었습니다.");
+	}
+
 }

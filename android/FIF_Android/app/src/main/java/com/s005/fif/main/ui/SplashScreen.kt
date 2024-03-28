1a1013f7550821227c2fc3565fdd08fbad1c69bc
최지찬 <pnlkc@naver.com>
2024-03-28 17:56:31 +0900

    [Android] Feat #55: 스플래시 화면에 앱 이름 추가

--- android/FIF_Android/app/src/main/java/com/s005/fif/main/ui/SplashScreen.kt
+++ android/FIF_Android/app/src/main/java/com/s005/fif/main/ui/SplashScreen.kt
@@ -2,12 +2,14 @@ package com.s005.fif.main.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
@@ -16,9 +18,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.s005.fif.R
+import com.s005.fif.ui.theme.Typography
 import com.s005.fif.user.ui.UserViewModel
 
 @Composable
@@ -42,20 +47,31 @@ fun SplashScreen(
         }
     }
 
-    Box(
+    Column(
         modifier = modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.spacedBy(
+            space = 20.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         // TODO. 앱 아이콘으로 이미지 변경 필요
         Image(
             modifier = Modifier
-                .size(100.dp)
-                .align(Alignment.Center),
+                .size(100.dp),
             painter = painterResource(id = R.drawable.account),
             contentDescription = "",
             colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
             contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = Typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold
         )
     }
 }
